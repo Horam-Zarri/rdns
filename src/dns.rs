985b6d65651ec1958f0571d39a73f6561255ff05
Horam Zarri <horamzarri@gmail.com>
2024-02-15 19:08:24 +0330

    Initial commit

---
+++ src/dns.rs
@@ -0,0 +1,91 @@
+pub mod interface;
+
+use std::fmt::{Debug, Display, Formatter, write};
+use std::net::{AddrParseError, IpAddr, Ipv4Addr, Ipv6Addr};
+use std::str::FromStr;
+use clap::Args;
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Args, Clone)]
+pub struct DnsServer {
+
+    /// Primary DNS address
+    primary: String,
+
+    /// Secondary DNS address
+    secondary: String,
+
+    /// Server name
+    pub name: String,
+
+    /// IPv6 DNS address (Default is v4)
+    #[arg(long)]
+    v6: bool,
+}
+
+impl DnsServer {
+
+    pub fn build(primary: String, secondary: String, v6: bool) -> Result<Self, AddrParseError> {
+        let dns = Self {primary, secondary, v6, name: "".to_string()};
+        dns.verify_dns()?;
+        Ok(dns)
+    }
+
+    pub fn verify_dns(&self) -> Result<(), AddrParseError> {
+        Ok({
+            if self.v6 {
+                Ipv6Addr::from_str(&self.primary)?;
+                Ipv6Addr::from_str(&self.secondary)?;
+            }
+            else {
+                Ipv4Addr::from_str(&self.primary)?;
+                Ipv4Addr::from_str(&self.secondary)?;
+            }
+        })
+    }
+
+    pub fn conflicts_with(&self, other: &Self) -> bool {
+        (self.primary == other.primary && self.secondary == other.secondary)
+        || self.name == other.name
+    }
+
+    pub fn set_dns(&self) {
+        if cfg!(windows) {
+            if let Err(e) = interface::set_static_windows(self) {
+                eprintln!("Can not set DNS :\n{e:?}");
+                std::process::abort();
+            }
+        }
+        else {
+            panic!("Only Windows is supported for now!");
+        }
+    }
+
+    pub fn set_dhcp(v6: bool) {
+        if cfg!(windows) {
+            if let Err(e) = interface::set_dhcp_windows(v6) {
+                eprintln!("Can not set DNS:\n{e:?}");
+                std::process::abort();
+            }
+        }
+        else {
+            panic!("Only Windows is supported for now!");
+        }
+    }
+}
+
+impl Display for DnsServer {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}:\tPrimary = [{}]\tSecondary = [{}]\tIPv{}",
+            self.name, self.primary, self.secondary,
+            if self.v6 {"6"} else {"4"})
+    }
+}
+
+
+
+
+
+
+
+
---
+++ src/main.rs
@@ -0,0 +1,148 @@
+mod dns;
+
+use std::str::FromStr;
+use std::io::{Read, Write};
+use clap::Parser;
+use clap::{Args, Subcommand};
+use crate::dns::DnsServer;
+use crate::dns::interface::server_list;
+
+const CONFIG_FILE: &str = "rdns_servers.json";
+
+#[derive(Parser)]
+#[command(name = "rdns")]
+#[command(version = "0.1.0")]
+#[command(about, long_about = None)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+
+}
+
+
+#[derive(Subcommand)]
+pub enum Commands {
+    /// Set a DNS server from your list
+    Set {
+        /// DNS server name
+        name: String,
+    },
+
+    /// Add a DNS server to your list
+    Add(DnsServer),
+
+    /// Remove a DNS server from your list
+    Rem {
+        /// DNS server name
+        names: Vec<String>,
+    },
+
+    /// List your saved DNS servers
+    List,
+
+    /// Directly set a DNS (or clear to DHCP)
+    #[group(required = true)]
+    Direct {
+        /// Primary DNS address
+        #[arg(conflicts_with = "dhcp", required_unless_present = "dhcp")]
+        primary: Option<String>,
+
+        /// Secondary DNS address
+        #[arg(conflicts_with = "dhcp", required_unless_present = "dhcp")]
+        secondary: Option<String>,
+
+        #[arg(long)]
+        /// IPv6 DNS address (Default is v4)
+        v6: bool,
+
+        /// Set to DHCP
+        #[arg(long)]
+        dhcp: bool,
+    }
+}
+
+
+
+fn main() {
+
+    let cli = Cli::parse();
+
+    match cli.command {
+        Commands::Set {name} => {
+            let servers = dns::interface::server_list();
+
+            servers.iter()
+                .find(|&dns| dns.name == name)
+                .expect("Specified DNS does not exist in list")
+                .set_dns();
+        }
+
+        Commands::Add(raw_dns) => {
+
+            if let Err(_) = DnsServer::verify_dns(&raw_dns) {
+                eprintln!("Invalid DNS address format!");
+                return;
+            }
+
+            let mut servers = dns::interface::server_list();
+
+            if servers.iter()
+                .find(|&dns| dns.conflicts_with(&raw_dns))
+                .is_some()
+            {
+                eprintln!("Another DNS with same name/addr exists!");
+                return;
+            }
+
+            servers.push(raw_dns);
+
+            dns::interface::write_servers(&servers);
+        }
+
+        Commands::Rem{names} => {
+
+            let mut servers = dns::interface::server_list();
+
+            for name in names {
+                if let Some((dns_pos, _)) = servers.iter()
+                    .enumerate()
+                    .find(|(idx, dns)| dns.name == name)
+                {
+                    servers.remove(dns_pos);
+                    println!("Removed DNS server: {name}");
+                }
+                else {
+                    println!("DNS server {name} not found in list!");
+                }
+            }
+
+            dns::interface::write_servers(&servers);
+        }
+        Commands::Direct {
+            primary,
+            secondary,
+            v6,
+            dhcp
+        } => {
+            if dhcp {
+                DnsServer::set_dhcp(v6);
+            }
+            else {
+                let dns_res = DnsServer::build(
+                    primary.expect("clap should avoid empty servers when DHCP is not set"),
+                    secondary.expect("clap should avoid empty servers when DHCP is not set"),
+                    v6
+                );
+                match dns_res {
+                    Ok(dns) => dns.set_dns(),
+                    Err(e) => eprintln!("Invalid DNS address format!"),
+                }
+            }
+        }
+
+        Commands::List => server_list().iter()
+            .for_each(|dns| println!("{dns}")),
+    }
+}
+
+
